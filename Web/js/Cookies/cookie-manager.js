81951d4f89e6488734d4d34cca1791a2e0200d43
MrtnNmck <mrtn.nemcek@gmail.com>
2015-11-02 01:55:05 +0100

    Pridanie path do vymazavania cookies

--- Web/js/Cookies/cookie-manager.js
+++ Web/js/Cookies/cookie-manager.js
@@ -44,6 +44,6 @@ function deleteAllCookies() {
     document.cookie.split(";").forEach(function(cookie) {
         var eqPos = cookie.indexOf("=");
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
     });
 }
