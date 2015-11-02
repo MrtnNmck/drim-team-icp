1d52df3b77a07c3966984d4df497422a3137ae70
MrtnNmck <mrtn.nemcek@gmail.com>
2015-11-02 01:51:04 +0100

    Vymazanie vsetkych cookies
    Resetnutie cookies pri zacati vyplnania formularov.

--- Web/js/Cookies/cookie-manager.js
+++ Web/js/Cookies/cookie-manager.js
@@ -38,3 +38,12 @@ function checkCookie(name) {
         return false;
     }
 }
+
+function deleteAllCookies() {
+
+    document.cookie.split(";").forEach(function(cookie) {
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    });
+}
