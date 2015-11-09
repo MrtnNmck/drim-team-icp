19c1350f63a4d00d4798af11636fc0745f7e79f8
MrtnNmck <mrtn.nemcek@gmail.com>
2015-11-09 12:25:56 +0100

    Validacia a ukladanie hofnot formularov aj tlacidlom spat

--- Web/js/Validator/validation.js
+++ Web/js/Validator/validation.js
@@ -66,41 +66,20 @@ $( document ).ready(function() {
     $("#nextButton").click(function(e) {
         e.preventDefault();
 
-        var validationValue = 0;
-        var inputs = document.getElementsByTagName("input");
-        var inputsList = Array.prototype.slice.call(inputs);
-
-        var radioDiv = null;
-        var inputCounter = 0;
-        var validInputCounter = 0;
-
-        inputsList.forEach(function(element) {
-            if ($(element).is(":radio") && $(element).is(":visible")) {
-                if (!($(element).closest("div").is(radioDiv))) {
-                    radioDiv = $(element).closest("div");
-                    inputCounter++;
-                    validInputCounter++;
-                } else {
-                    return;
-                }
-            } else if ($(element).is(":visible")) {
-                if (element.value.length > 0 && $(element).valid()) {
-                    inputCounter++;
-                    validInputCounter++;
-                } else {
-                    inputCounter++;
-                }
-            }
-        });
-
-        validationValue = Math.round(100 / inputCounter * validInputCounter);
-
-        localStorage.setItem("progress-bar." + getCurrentFileName(), validationValue);
-
-        serializeForm($("form")[0].id);
+        validateAllInputs();
 
         if ($("#" + e.currentTarget.dataset.submit).valid()) {
             window.location.href = (e.currentTarget.href);
+        }
+    });
+
+    $("#backButton").click(function(e) {
+        e.preventDefault();
+
+        validateAllInputs();
+
+        if ($("#" + e.currentTarget.dataset.submit).valid()) {
+            window.location.href = e.currentTarget.href;
         }
     });
 
@@ -135,4 +114,41 @@ function allRequiredInputsAreFilled() {
     }
 
     return true;
+}
+
+function validateAllInputs() {
+
+    var validationValue = 0;
+    var inputs = document.getElementsByTagName("input");
+    var inputsList = Array.prototype.slice.call(inputs);
+
+    var radioDiv = null;
+    var inputCounter = 0;
+    var validInputCounter = 0;
+
+    inputsList.forEach(function(element) {
+        if ($(element).is(":radio") && $(element).is(":visible")) {
+            if (!($(element).closest("div").is(radioDiv))) {
+                radioDiv = $(element).closest("div");
+                inputCounter++;
+                validInputCounter++;
+            } else {
+                return;
+            }
+        } else if ($(element).is(":visible")) {
+            if (element.value.length > 0 && $(element).valid()) {
+                inputCounter++;
+                validInputCounter++;
+            } else {
+                inputCounter++;
+            }
+        }
+    });
+
+    validationValue = Math.round(100 / inputCounter * validInputCounter);
+
+    localStorage.setItem("progress-bar." + getCurrentFileName(), validationValue);
+
+    serializeForm($("form")[0].id);
+
 }
